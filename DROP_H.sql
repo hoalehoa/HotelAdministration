5c13b267bcb7cb5bd9dd2af2b3a7337be6169cdf
Hoa <wi19b046@technikum-wien.at>
2021-06-12 09:43:15 +0200

    from Hoa

---
+++ DROP_H.sql
@@ -0,0 +1,26 @@
+-----------------------------
+--  DDL table for WarenArt
+-----------------------------
+select * from hotel;
+DROP TABLE WarenArt;
+DROP TABLE Ware;
+DROP TABLE Adresse;
+DROP TABLE Service;
+DROP TABLE Zimmerklasse;
+DROP TABLE Zimmer;
+DROP TABLE Land;
+DROP TABLE Reisepass;
+DROP TABLE Gehaltsstufe;
+DROP TABLE Abteilung;
+DROP TABLE Person;
+DROP TABLE Bankinformation;
+DROP TABLE Personal;
+DROP TABLE Gast;
+DROP TABLE Lieferanten;
+DROP TABLE Rechnung;
+DROP TABLE Bestellung;
+DROP TABLE Zimmerbuchung;
+DROP TABLE Bestelldetails;
+DROP TABLE Servicedetails;
+DROP TABLE Zimmerbuchungsdetails;
+DROP TABLE Kundenbewertung;
